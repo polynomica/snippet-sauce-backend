9bbd182fe75730ab1ae5987b3a957c40a411319f
Hetarth02 <hetarth02@gmail.com>
2021-10-20 03:48:46 +0530

    distributed code according to its functionality into different Controllers simplified CodeController methods Added new UserController to handle all authentication Improved Authentication Logic, more secure and optimized Database should store hashed/encrypted values of passwords and Authentication will be done using the same

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Code;
+use App\Models\Users;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Validator;
+
+class UserController extends Controller
+{
+    public function author_details($git_username)
+    {
+        //Fetch author details using github REST API
+        $url_string = 'https://api.github.com/users/'.$git_username;
+        $response = Http::get($url_string);
+        $author_info = ["author_info" => array($response['login'], $response['bio'], $response['avatar_url'], $response['html_url'])];
+        return response()->json($author_info);
+    }
+
+    public function author_login(Request $request)
+    {
+        $input = $request->all();
+
+        //Validation
+        $validator = Validator::make($input, [
+            'git_username' => 'required',
+            'password' => 'required',
+        ]);
+        if ($validator->fails()) {
+            return response()->json(['alert' => 'Please fill out empty fields']);
+        } else {
+            $git_username = $input['git_username'];
+            $password = $input['password'];
+
+            //Authenciate user credentials
+            $data = array(
+                'username' => $git_username,
+                'password' => $password
+            );
+            if (Auth::attempt($data)) {
+                $url_string = 'https://api.github.com/users/'.$git_username;
+                $response = Http::get($url_string);
+                $author_info = ["author_info" => array($response['login'], $response['bio'], $response['avatar_url'], $response['html_url'])];
+                return response()->json([$author_info]);
+            } else {
+                return response()->json(['alert' => 'Wrong Credentials, Please try again!']);
+            }
+        }
+    }
+}
